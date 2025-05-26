a19964ec20b05d8a1b9696fd39e21a459373781f
gKrokod <t9013054362@gmail.com>
2025-05-26 15:14:24 +0300

    book store

--- src/Api.hs
+++ src/Api.hs
@@ -1,9 +1,5 @@
-{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE FlexibleContexts #-}
-
 
 module Api where
 
@@ -17,16 +13,14 @@ data Book = Book
   { bookId    :: Int
   , bookTitle :: Text
   , bookAuthor :: Text
-  } deriving (Show, Eq, Generic)
-
-instance ToJSON Book
-instance FromJSON Book
+  } 
+  deriving stock (Show, Eq, Generic)
+  deriving anyclass (ToJSON, FromJSON)
 
 -- Определение API
 type BookAPI =
        "books" :> Get '[JSON] [Book]          -- Список книг
   :<|> "books" :> Capture "id" Int :> Get '[JSON] Book -- Книга по ID
   :<|> "books" :> ReqBody '[JSON] Book :> Post '[JSON] Book -- Добавление книги
-  -- :<|> "books" :> Capture "id" Int :> DeleteNoContent '[JSON] NoContent -- Удаление книги
   :<|> "books" :> Capture "id" Int :> DeleteNoContent
 
