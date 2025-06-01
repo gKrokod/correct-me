e1f322d6d2c1a23f7136d3b18f80e55d6dde4abe
gKrokod <t9013054362@gmail.com>
2025-06-01 04:36:54 -0700

    add timeout

--- src/Web/Yandex.hs
+++ src/Web/Yandex.hs
@@ -9,6 +9,8 @@ import Data.Text.Encoding (encodeUtf8)
 import GHC.Generics (Generic)
 import Network.HTTP.Simple
 import Web.Types (SpellResult)
+import Control.Concurrent.Async (race)
+import Control.Concurrent (threadDelay)
 
 data ConfigYandexService = MkConfigYandexService
   { cHost :: Text,
@@ -22,17 +24,22 @@ data ConfigYandexService = MkConfigYandexService
 
 revisionSpell :: ConfigYandexService -> Text -> IO (Either Text SpellResult)
 revisionSpell cfg txt = do
-  response' <- try @SomeException . httpLBS . buildGetRequest cfg $ txt
-  case response' of
-    Left e -> pure . Left . T.pack . displayException $ e
-    Right response -> do
-      let status = getResponseStatusCode response
-      let body = eitherDecode @SpellResult $ getResponseBody response
-      case (status, body) of
-        (404, _) -> pure $ Left "spellservice 404"
-        (301, _) -> pure $ Left "spellservice 301"
-        (_, Right spellresult) -> pure $ Right spellresult
-        _ -> pure $ Left $ "error:" <> T.pack (show status) <> T.pack (show body)
+  timeout <- race (threadDelay 10000000) runYandexService 
+  case timeout of
+    Left _ -> pure $ Left "timeout"
+    Right r -> pure r
+  where runYandexService = do
+          response' <- try @SomeException . httpLBS . buildGetRequest cfg $ txt
+          case response' of
+            Left e -> pure . Left . T.pack . displayException $ e
+            Right response -> do
+              let status = getResponseStatusCode response
+              let body = eitherDecode @SpellResult $ getResponseBody response
+              case (status, body) of
+                (404, _) -> pure $ Left "spellservice 404"
+                (301, _) -> pure $ Left "spellservice 301"
+                (_, Right spellresult) -> pure $ Right spellresult
+                _ -> pure $ Left $ "error:" <> T.pack (show status) <> T.pack (show body)
 
 -- JSON-интерфейс:
 -- https://speller.yandex.net/services/spellservice.json/checkText?text=синхрафазатрон+в+дубне
